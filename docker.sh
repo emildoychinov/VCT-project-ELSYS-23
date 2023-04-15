148cb057ec315084b48279b7ce9dc29404c2f393
Aalyth <d.nikolov7654@gmail.com>
2023-04-15 21:09:06 +0300

    docker.sh comment cleanup

--- docker.sh
+++ docker.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-#docker swarm init
-# this creates the vot-project_chat image
-#docker build -t chat-img -f Dockerfile.server .
-
 docker compose up -d 
-# this itself runs the docker-compose.yml file to build the needed replicas
-#docker stack deploy --compose-file docker-compose.yml vot-project
-
-#docker network create cassandra
-#docker run --rm -d --name cassandra --hostname cassandra --network cassandra cassandra
-
